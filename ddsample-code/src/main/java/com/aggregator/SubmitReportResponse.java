4fb3e3ce50e673b70295da0f3913f1722fba7f1e
radu-duca <radu.mihai.duca@gmail.com>
2016-07-19 11:25:04 -0700

    modificare

--- ddsample-code/src/main/java/com/aggregator/SubmitReportResponse.java
+++ ddsample-code/src/main/java/com/aggregator/SubmitReportResponse.java
@@ -25,7 +25,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "SubmitReportResponse")
+@XmlType(name = "submitReportResponse")
 public class SubmitReportResponse {
 
 
