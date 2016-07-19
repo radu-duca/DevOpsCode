24c27d30c4ba7bacbc63f5c12671ffaa64726bf2
radu-duca <radu.mihai.duca@gmail.com>
2016-07-19 11:13:33 -0700

    modificare

--- ddsample-code/src/main/java/com/aggregator/SubmitReport.java
+++ ddsample-code/src/main/java/com/aggregator/SubmitReport.java
@@ -26,7 +26,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "SubmitReport", propOrder = {
+@XmlType(name = "submitReport", propOrder = {
     "arg0"
 })
 public class SubmitReport {
